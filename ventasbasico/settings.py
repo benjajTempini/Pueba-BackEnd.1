7282f2ba95df8084f3ee48c1dcb57a973ae03820
BenjajTempini <benjamin.rojas71@inacapmail.cl>
2025-10-28 13:55:05 -0300

    vamos de nuevo

--- ventasbasico/settings.py
+++ ventasbasico/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-a!)1(l*2=!gtiys$g6i^4p4d9b%60z^el0v$)0&6%d1nyr&kd#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".onrender.com",
