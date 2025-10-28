a87ea08ab25c2464d435aec9c9ab65fde8c08f4e
benjajTempini <benjamin.rojas71@inacapmail.cl>
2025-10-28 00:25:14 -0300

    redi

--- ventasbasico/admin.py
+++ ventasbasico/admin.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
 from .models import Productos, Venta, DetalleVenta
-from django.http import HttpResponse
-import csv
-
 
 @admin.register(Productos)
 class ProductosAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'codigo', 'precio', 'stock')
-    list_filter = ('nombre','precio', 'stock')
+    list_filter = ('precio', 'stock')
     search_fields = ('nombre', 'codigo')
     ordering = ('nombre',)
 
@@ -32,13 +29,3 @@ class VentaAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         # Evitar que se puedan eliminar ventas desde el admin
         return False
-    
-
-
-    
-#@admin.action(description="EXPORTAR VENTA DE PRODCUTOS A CSV")
-#def exportar_productos_csv(modelAdmin,request,queryset):
-#    response = HttpResponse(content_type='text/csv')
-#    response['Content-Disposicion'] = 'attachment; filename="productos.csv"'
-#    archivo = csv.writer(response)
-#    archivo.writerow(['nombre','precio','stock','activo'])
