329af37496677ff1e51b689c89c911bd19c6df93
benjajTempini <benjamin.rojas71@inacapmail.cl>
2025-11-22 02:12:01 -0300

    readiiii tambienene

--- ventasbasico/serializers.py
+++ ventasbasico/serializers.py
@@ -1,18 +1,21 @@
 from rest_framework import serializers
 from .models import *
+from clientes.serializers import ClienteSerializer
 
 
-class ProductosSerializer(serializers.HyperlinkedModelSerializer):
+class ProductosSerializer(serializers.ModelSerializer):
     class Meta:
         model = Productos
         fields = ["nombre","codigo","stock","precio"]
 
-class VentaSerializer(serializers.HyperlinkedModelSerializer):
+class VentaSerializer(serializers.ModelSerializer):
+    rut_cliente = ClienteSerializer(read_only=True)
+    
     class Meta:
         model = Venta
         fields = ["numero","fecha","rut_cliente","total"]
 
-class DetalleVentaSerializer(serializers.HyperlinkedModelSerializer):
+class DetalleVentaSerializer(serializers.ModelSerializer):
     class Meta:
         model = DetalleVenta
         fields = ["venta","producto","cantidad","precio_unitario"]
